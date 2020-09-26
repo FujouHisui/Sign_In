7db04b760b57faa3f978b0ca883844b2aee8d24b
Allen-1107 <llc530144946@gmail.com>
2020-09-26 14:06:57 +0800

    new layout fix

--- app/src/main/java/tech/hisui/sign_in/LoginActivity.java
+++ app/src/main/java/tech/hisui/sign_in/LoginActivity.java
@@ -1,8 +1,10 @@
 package tech.hisui.sign_in;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
